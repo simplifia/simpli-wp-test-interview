306a6caa96bc8a7f7500a93f9b67cdb0a1b93d2f
mayssoun1 <mayssa.bensalah3@gmail.com>
2020-12-16 18:55:33 +0100

    adding new branch for Mayssa ben salah

---
+++ simpli-wp-ceremony-streaming-entretien/Views/template.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Formulaire en ligne</title>
+</head>
+<style>
+    form {
+  /* Uniquement centrer le formulaire sur la page */
+  margin: 0 auto;
+  width: 400px;
+  /* Encadré pour voir les limites du formulaire */
+  padding: 1em;
+  border: 1px solid #CCC;
+  border-radius: 1em;
+}
+
+form div + div {
+  margin-top: 1em;
+}
+
+label {
+  /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
+  display: inline-block;
+  width: 90px;
+  text-align: right;
+}
+
+input, textarea {
+  /* Pour s'assurer que tous les champs texte ont la même police.
+     Par défaut, les textarea ont une police monospace */
+  font: 1em sans-serif;
+
+  /* Pour que tous les champs texte aient la même dimension */
+  width: 300px;
+  box-sizing: border-box;
+
+  /* Pour harmoniser le look & feel des bordures des champs texte */
+  border: 1px solid #999;
+}
+
+input:focus, textarea:focus {
+  /* Pour souligner légèrement les éléments actifs */
+  border-color: #000;
+}
+
+textarea {
+  /* Pour aligner les champs texte multi‑ligne avec leur étiquette */
+  vertical-align: top;
+
+  /* Pour donner assez de place pour écrire du texte */
+  height: 5em;
+}
+
+.button {
+  /* Pour placer le bouton à la même position que les champs texte */
+  padding-left: 330px; /* même taille que les étiquettes */
+}
+
+button {
+  /* Cette marge supplémentaire représente grosso modo le même espace que celui
+     entre les étiquettes et les champs texte */
+  margin-left: .5em;
+}
+</style>
+<body>
+<form action="/action.php" method="post">
+   <p>Un formulaire pour créer un post et ses metadata</p>
+    <div>
+        <label for="postname">Post name :</label>
+        <input type="text" id="postname" name="postname" placeholder="TEXT">
+    </div>
+    <div>
+        <label for="postcontent">Post content:</label>
+        <input type="text" id="postcontent" name="postname" placeholder="TEXT">
+    </div>
+    <div>
+        <label for="metadata">Metadata :my meta</label>
+        <input type="text" id="metadata" name="metadata" placeholder="TEXT">
+    </div>
+
+    <div class="button">
+        <button type="submit">Submit</button>
+    </div>
+</form>
+
+</body>
