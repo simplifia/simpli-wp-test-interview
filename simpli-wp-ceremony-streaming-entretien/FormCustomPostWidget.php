b20f2306d38b5e6d698522830b32ab22148f0f28
ag7 <alexis26999@gmail.com>
2020-12-21 16:01:24 +0100

    insert post in db on submit

--- simpli-wp-ceremony-streaming-entretien/FormCustomPostWidget.php
+++ simpli-wp-ceremony-streaming-entretien/FormCustomPostWidget.php
@@ -2,6 +2,7 @@
 
 namespace SimpliCeremonyStreamingPlugin;
 
+use SimpliCeremonyStreamingPlugin\Models\CustomPost;
 use SimpliCeremonyStreamingPlugin\Models\View;
 
 class FormCustomPostWidget
@@ -23,6 +24,18 @@ class FormCustomPostWidget
 
     public function form()
     {
+        // if form submited insert data in db
+        if (isset($_POST) && isset($_POST['submit'])) {
+            // get id of inserted post
+            $newPost = new CustomPost(
+                [
+                    'postName' => $_POST['post_name'],
+                    'postContent' => $_POST['post_content'],
+                    'postMeta' => $_POST['post_meta']
+                ]
+            );
+            $newPost->save();
+        }
         $this->formPostView = new View(plugin_dir_path(__FILE__) . 'Views/FormCustomPostView.php', []);
         $this->formPostView->Render(array('broadcast' => null));
     }
